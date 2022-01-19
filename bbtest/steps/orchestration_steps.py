134aaaea830812880b57b798b9991c99ee9172ca
Jan Cajthaml <jan.cajthaml@gmail.com>
2022-01-19 23:43:09 +0100

    use openbank_testkit for tests

--- bbtest/steps/orchestration_steps.py
+++ bbtest/steps/orchestration_steps.py
@@ -2,24 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from behave import *
-from helpers.shell import execute
 from helpers.eventually import eventually
 import os
 import json
-import urllib.request
+from openbank_testkit import Shell, Request
 
 
 @given('package {package} is {operation}')
 def step_impl(context, package, operation):
   if operation == 'installed':
-    (code, result, error) = execute(["apt-get", "install", "-f", "-qq", "-o=Dpkg::Use-Pty=0", "-o=Dpkg::Options::=--force-confold", context.unit.binary])
+    (code, result, error) = Shell.run(["apt-get", "install", "-f", "-qq", "-o=Dpkg::Use-Pty=0", "-o=Dpkg::Options::=--force-confold", context.unit.binary])
     assert code == 'OK', "unable to install with code {} and {} {}".format(code, result, error)
     assert os.path.isfile('/etc/data-warehouse/conf.d/init.conf') is True, 'config file does not exists'
-    execute(['systemctl', 'start', package])
+    Shell.run(['systemctl', 'start', package])
   elif operation == 'uninstalled':
-    (code, result, error) = execute(["apt-get", "-f", "-qq", "remove", package])
+    (code, result, error) = Shell.run(["apt-get", "-f", "-qq", "remove", package])
     assert code == 'OK', "unable to uninstall with code {} and {} {}".format(code, result, error)
-    (code, result, error) = execute(["apt-get", "-f", "-qq", "purge", package])
+    (code, result, error) = Shell.run(["apt-get", "-f", "-qq", "purge", package])
     assert code == 'OK', "unable to purge with code {} and {} {}".format(code, result, error)
     assert os.path.isfile('/etc/data-warehouse/conf.d/init.conf') is False, 'config file still exists'
   else:
@@ -29,7 +28,7 @@ def step_impl(context, package, operation):
 @given('systemctl contains following active units')
 @then('systemctl contains following active units')
 def step_impl(context):
-  (code, result, error) = execute(["systemctl", "list-units", "--all", "--no-legend"])
+  (code, result, error) = Shell.run(["systemctl", "list-units", "--all", "--no-legend"])
   assert code == 'OK', str(result) + ' ' + str(error)
 
   items = []
@@ -45,7 +44,7 @@ def step_impl(context):
 @given('systemctl does not contain following active units')
 @then('systemctl does not contain following active units')
 def step_impl(context):
-  (code, result, error) = execute(["systemctl", "list-units", "--all", "--no-legend"])
+  (code, result, error) = Shell.run(["systemctl", "list-units", "--all", "--no-legend"])
   assert code == 'OK', str(result) + ' ' + str(error)
 
   items = []
@@ -63,14 +62,15 @@ def step_impl(context):
 def unit_running(context, unit):
   @eventually(10)
   def wait_for_unit_state_change():
-    (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
+    (code, result, error) = Shell.run(["systemctl", "show", "-p", "SubState", unit])
     assert code == 'OK', str(result) + ' ' + str(error)
     assert 'SubState=running' in result, result
 
+  request = Request(method='GET', url="http://127.0.0.1/health")
+
   @eventually(10)
   def wait_for_service_to_be_healthy():
-    request = urllib.request.Request(method='GET', url= "http://127.0.0.1/health")
-    response = urllib.request.urlopen(request, timeout=2)
+    response = request.do()
     assert response.status == 200, str(response.status)
     status = json.loads(response.read().decode('utf-8'))
     assert status['healthy'] is True, 'unit is not healthy'
@@ -84,7 +84,7 @@ def unit_running(context, unit):
 def unit_not_running(context, unit):
   @eventually(10)
   def wait_for_unit_state_change():
-    (code, result, error) = execute(["systemctl", "show", "-p", "SubState", unit])
+    (code, result, error) = Shell.run(["systemctl", "show", "-p", "SubState", unit])
     assert code == 'OK', str(result) + ' ' + str(error)
     assert 'SubState=dead' in result, result
 
@@ -94,7 +94,7 @@ def unit_not_running(context, unit):
 @given('{operation} unit "{unit}"')
 @when('{operation} unit "{unit}"')
 def operation_unit(context, operation, unit):
-  (code, result, error) = execute(["systemctl", operation, unit])
+  (code, result, error) = Shell.run(["systemctl", operation, unit])
   assert code == 'OK', str(result) + ' ' + str(error)
 
 
